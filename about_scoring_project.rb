fb671591a3e9392272ae5be42d84a1e878d04b0e
therealloker <therealloker@gmail.com>
2018-04-28 18:56:40 +0300

    255 tests completed

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -30,7 +30,22 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # Your goal is to write the score method.
 
 def score(dice)
-  # You need to write this method
+  score = 0
+  totals = [0] * 7
+
+  dice.map { |i| totals[i] += 1 }
+
+  for i in (1..6)
+    if totals[i] >= 3
+      if i == 1
+        score += 1000
+      else
+        score += 100 * i
+      end
+      totals[i] -= 3
+    end
+  end
+  score += (totals[1] * 100 + totals[5] * 50)
 end
 
 class AboutScoringProject < Neo::Koan
